1302c5bffdc0651d7505936a0af2ac480d5d0f39
Philip T Dodd <phil.dodd@cinnamonapps.co.uk>
2023-03-01 23:26:51 +0000

    red:Added Card enum, fails tests

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -1,5 +1,39 @@
 package org.example;
 
+public enum Card {
+    C2("2", 2, 2),
+    C3("3", 2, 2),
+    C4("4", 3, 3),
+    C5("5", 5, 5),
+    C6("6", 6, 6),
+    C7("7", 7, 7),
+    C8("8", 8, 8),
+    C9("9", 9, 9),
+    CT("T", 10, 10),
+    CJ("J", 11, 10),
+    CQ("Q", 12, 10),
+    CK("K", 13, 10),
+    CA("A", 14, 11);
 
-public class Card {
+    private final String letter;
+    private final int order;
+    private final int faceValue;
+
+    Card(String letter, int order, int faceValue) {
+        this.letter = letter;
+        this.order = order;
+        this.faceValue = faceValue;
+    }
+
+    public int getFaceValue() {
+        return faceValue;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
 }
--- src/test/java/org/example/CardTest.java
+++ src/test/java/org/example/CardTest.java
@@ -1,7 +1,88 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CardTest {
+    @ParameterizedTest
+    @EnumSource(value = Card.class)
+    void valueOfCard(Card card) {
+        String expectedLetter = null;
+        int expectedOrder = 0;
+        int expectedFaceValue = 0;
 
+        switch (card) {
+            case C2 -> {
+                expectedLetter = "2";
+                expectedFaceValue = 2;
+                expectedOrder = 2;
+            }
+            case C3 -> {
+                expectedLetter = "3";
+                expectedFaceValue = 3;
+                expectedOrder = 3;
+            }
+            case C4 -> {
+                expectedLetter = "4";
+                expectedFaceValue = 4;
+                expectedOrder = 4;
+            }
+            case C5 -> {
+                expectedLetter = "5";
+                expectedFaceValue = 5;
+                expectedOrder = 5;
+            }
+            case C6 -> {
+                expectedLetter = "6";
+                expectedFaceValue = 6;
+                expectedOrder = 6;
+            }
+            case C7 -> {
+                expectedLetter = "7";
+                expectedFaceValue = 7;
+                expectedOrder = 7;
+            }
+            case C8 -> {
+                expectedLetter = "8";
+                expectedFaceValue = 8;
+                expectedOrder = 8;
+            }
+            case C9 -> {
+                expectedLetter = "9";
+                expectedFaceValue = 9;
+                expectedOrder = 9;
+            }
+            case CT -> {
+                expectedLetter = "T";
+                expectedFaceValue = 10;
+                expectedOrder = 10;
+            }
+            case CJ -> {
+                expectedLetter = "J";
+                expectedFaceValue = 10;
+                expectedOrder = 11;
+            }
+            case CQ -> {
+                expectedLetter = "Q";
+                expectedFaceValue = 10;
+                expectedOrder = 12;
+            }
+            case CK -> {
+                expectedLetter = "K";
+                expectedFaceValue = 10;
+                expectedOrder = 13;
+            }
+            case CA -> {
+                expectedLetter = "A";
+                expectedFaceValue = 11;
+                expectedOrder = 14;
+            }
+        }
+
+        assertEquals(expectedLetter, card.getLetter());
+        assertEquals(expectedFaceValue, card.getFaceValue());
+        assertEquals(expectedOrder, card.getOrder());
+    }
 }
